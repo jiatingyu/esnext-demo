9c9d1b43bf9dccdbb52942ac930285e08f6331e0
jiatingyu <jiatingyu94@gmail.com>
2024-07-10 22:59:22 +0800

    ..

--- 00-继承.js
+++ 00-继承.js
@@ -1,63 +1,88 @@
-// 原型链继承
+// // 原型链继承
 
-// 优点： 父类新增原型方法或属性，子类都能访问到，可共用方法
-// 缺点：
-// + 创建子类实例时，无法向父类构造函数传参。
-// + 引用类型的属性会被所有实例共享
-//  因为两个实例使用的是同一个原型对象。它们的内存空间是共享的，当一个发生变化的时候，另外一个也随之进行了变化，这就是使用原型链继承方式的一个缺点。
+// // 优点： 父类新增原型方法或属性，子类都能访问到，可共用方法
+// // 缺点：
+// // + 创建子类实例时，无法向父类构造函数传参。
+// // + 引用类型的属性会被所有实例共享
+// //  因为两个实例使用的是同一个原型对象。它们的内存空间是共享的，当一个发生变化的时候，另外一个也随之进行了变化，这就是使用原型链继承方式的一个缺点。
 
-// function Parent() {
-//   this.name = "Parent";
-//   this.arr=[]
-// }
-// Parent.prototype.sayHello = function () {
-//   this.arr.push(1)
-//   console.log("Hello from " + this.name);
-// };
+// // function Parent() {
+// //   this.name = "Parent";
+// //   this.arr=[]
+// // }
+// // Parent.prototype.sayHello = function () {
+// //   this.arr.push(1)
+// //   console.log("Hello from " + this.name);
+// // };
 
-// function Child() {
-//   this.name = "Child";
-// }
+// // function Child() {
+// //   this.name = "Child";
+// // }
 
-// // 设置Child的原型为Parent的实例
-// Child.prototype = new Parent();
+// // // 设置Child的原型为Parent的实例
+// // Child.prototype = new Parent();
 
-// // 创建Child的实例
-// let child = new Child();
-// let child2 = new Child();
-// child.sayHello(); // 输出: Hello from Child
-// child.sayHello(); // 输出: Hello from Child
-// console.log(child.sayHello  === child2.sayHello); // true
-// console.log(child.arr,child2.arr); // [ 1, 1 ] [ 1, 1 ]
+// // // 创建Child的实例
+// // let child = new Child();
+// // let child2 = new Child();
+// // child.sayHello(); // 输出: Hello from Child
+// // child.sayHello(); // 输出: Hello from Child
+// // console.log(child.sayHello  === child2.sayHello); // true
+// // console.log(child.arr,child2.arr); // [ 1, 1 ] [ 1, 1 ]
 
 
-// 构造函数继承
-// 优点：可以向父类构造函数传参，解决原型链继承无法传参的问题。
-// 缺点：每次创建子类实例时都会创建父类的方法，（无法实现函数复用），导致内存浪费
-// function Parent(name) {
-//   this.name = name;
-//   this.arr=[]
-// }
-// Parent.prototype.sayHello = function () {
-//   this.arr.push(1)
-//   console.log("Hello from " + this.name);
-// };
+// // 构造函数继承
+// // 优点：可以向父类构造函数传参，解决原型链继承无法传参的问题。
+// // 缺点：每次创建子类实例时都会创建父类的方法，（无法实现函数复用），导致内存浪费
+// // function Parent(name) {
+// //   this.name = name;
+// //   this.arr=[]
+// // }
+// // Parent.prototype.sayHello = function () {
+// //   this.arr.push(1)
+// //   console.log("Hello from " + this.name);
+// // };
 
-// function Child(name) {
-//   Parent.call(this, name); // 调用Parent的构造函数
-// }
+// // function Child(name) {
+// //   Parent.call(this, name); // 调用Parent的构造函数
+// // }
 
-// Child.prototype = new Parent(); // 继承Parent的原型方法
-// Child.prototype.constructor = Child; // 修正构造函数指向
+// // Child.prototype = new Parent(); // 继承Parent的原型方法
+// // Child.prototype.constructor = Child; // 修正构造函数指向
 
-// var child = new Child('ziyu');
-// var child1 = new Child('ziyu');
-// console.log(child.sayHello()); 
-// console.log(child.sayHello()); 
-// console.log(child.arr, child1.arr); // 输出: [ 1, 1 ] []
+// // var child = new Child('ziyu');
+// // var child1 = new Child('ziyu');
+// // console.log(child.sayHello()); 
+// // console.log(child.sayHello()); 
+// // console.log(child.arr, child1.arr); // 输出: [ 1, 1 ] []
 
-// 优点: 可以传参，并且可以复用父类方法,可以避免引用类型属性共享的问题。
-// 缺点: 父类构造函数被调用了两次，一次在设置原型时，一次在构造函数中，可能会导致不必要性能开销。
+// // 优点: 可以传参，并且可以复用父类方法,可以避免引用类型属性共享的问题。
+// // 缺点: 父类构造函数被调用了两次，一次在设置原型时，一次在构造函数中，可能会导致不必要性能开销。
+// // function Parent(name) {
+// //   this.name = name;
+// // }
+// // Parent.prototype.sayHello = function() {
+// //   console.log('Hello from ' + this.name);
+// // };
+
+// // function Child(name, age) {
+// //   Parent.call(this, name); // 调用Parent的构造函数 ,   第一次调用
+// //   this.age = age;
+// // }
+
+// // // 通过原型链继承Parent的方法
+// // Child.prototype = Object.create(Parent.prototype);    // 第二次调用
+// // Child.prototype.constructor = Child;
+
+// // Child.prototype.sayBye = function() {
+// //   console.log('Bye from ' + this.name);
+// // };
+
+// // var child = new Child('ziyu', 25);
+// // child.sayHello(); // 输出: Hello from ziyu
+// // child.sayBye(); // 输出: Bye from ziyu
+
+
 // function Parent(name) {
 //   this.name = name;
 // }
@@ -66,12 +91,13 @@
 // };
 
 // function Child(name, age) {
-//   Parent.call(this, name); // 调用Parent的构造函数 ,   第一次调用
+//   // 创建一个没有实例的Parent对象
+//   var fakeParent = Object.create(Parent.prototype);
+//   Parent.call(fakeParent, name);
 //   this.age = age;
 // }
 
-// // 通过原型链继承Parent的方法
-// Child.prototype = Object.create(Parent.prototype);    // 第二次调用
+// Child.prototype = Object.create(Parent.prototype);
 // Child.prototype.constructor = Child;
 
 // Child.prototype.sayBye = function() {
@@ -82,51 +108,42 @@
 // child.sayHello(); // 输出: Hello from ziyu
 // child.sayBye(); // 输出: Bye from ziyu
 
+// // class Parent {
+// //   constructor(name) {
+// //     this.name = name;
+// //   }
+// //   sayHello() {
+// //     console.log('Hello from ' + this.name);
+// //   }
+// // }
 
-function Parent(name) {
-  this.name = name;
+// // class Child extends Parent {
+// //   constructor(name, age) {
+// //     super(name); // 调用父类的构造函数
+// //     this.age = age;
+// //   }
+// //   sayBye() {
+// //     console.log('Bye from ' + this.name);
+// //   }
+// // }
+
+// // var child = new Child('Kimi', 25);
+// // child.sayHello(); // 输出: Hello from Kimi
+// // child.sayBye(); // 输出: Bye from Kimi
+
+
+function* asyncGenerator() {
+  yield Promise.resolve(1);
+  yield Promise.resolve(2);
+  yield Promise.resolve(3);
 }
-Parent.prototype.sayHello = function() {
-  console.log('Hello from ' + this.name);
-};
 
-function Child(name, age) {
-  // 创建一个没有实例的Parent对象
-  var fakeParent = Object.create(Parent.prototype);
-  Parent.call(fakeParent, name);
-  this.age = age;
+ function useAsyncGenerator() {
+ for  (const value of asyncGenerator()) {
+   value.then(e=>{
+    console.log(e);
+   })
+ }
 }
 
-Child.prototype = Object.create(Parent.prototype);
-Child.prototype.constructor = Child;
-
-Child.prototype.sayBye = function() {
-  console.log('Bye from ' + this.name);
-};
-
-var child = new Child('ziyu', 25);
-child.sayHello(); // 输出: Hello from ziyu
-child.sayBye(); // 输出: Bye from ziyu
-
-// class Parent {
-//   constructor(name) {
-//     this.name = name;
-//   }
-//   sayHello() {
-//     console.log('Hello from ' + this.name);
-//   }
-// }
-
-// class Child extends Parent {
-//   constructor(name, age) {
-//     super(name); // 调用父类的构造函数
-//     this.age = age;
-//   }
-//   sayBye() {
-//     console.log('Bye from ' + this.name);
-//   }
-// }
-
-// var child = new Child('Kimi', 25);
-// child.sayHello(); // 输出: Hello from Kimi
-// child.sayBye(); // 输出: Bye from Kimi
+useAsyncGenerator();
---
+++ promise.js
@@ -0,0 +1,59 @@
+
+// 实现步骤
+// 1. 新建Promise需要使用new关键字，那他肯定是作为面向对象的方式调用的，Promise是一个类。
+// 2. new Promise(fn) 的时候需要传一个函数进去，说明Promise的参数是一个函数
+// 3. 构造函数传进去的fn会收到resolve和reject两个函数，用来表示Promise成功和失败，说明构造函数里面还需要resolve和reject这两个函数，这两个函数的作用是改变Promise的状态。
+// 4. 根据规范，promise 有 pending，fulfilled，rejected三个状态，初始状态为pending，调用resolve会将其改为fulfilled，调用reject会改为rejected。
+// 5. promise实例对象建好后可以调用then方法，说明then是一个实例方法。而且then方法可以链式调用，说明他很方法返回一个新的promise实例,(原生Promise不能自己返回自己)
+
+// 初始三个变量
+const PENDING = 'pending' ;
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
+class MyPromise {
+    // 初始化参数
+    state = PENDING; 
+    value= null;
+    reason = "";
+    // 构造函数
+    constructor (exec){
+        // 传递两个函数,绑定 this 避免 this指向出错
+        try {
+            exec(this.resolve.bind(this),this.reject.bind(this))
+        } catch (error) {
+            this.reject(error.message)
+        }
+    }
+    resolve(value){
+        // 判断状态, 修改状态 并赋值
+        if(this.state === PENDING){
+            this.state = FULFILLED
+            this.value = value
+        }
+    }
+    reject(reason){
+        // 判断状态, 修改状态 并赋值
+        if(this.state === PENDING){
+            this.state = REJECTED
+            this.reason = reason
+        }
+    }
+
+    then(onFulfilled, onRejected){
+        onFulfilled = onFulfilled ? onRejected : (value)=> value
+        onRejected = onRejected ? onRejected : (reason)=> { throw Error(reason) }
+        // 返回一个新的实例
+        console.log(this);
+        let p = new Promise((resolve,reject)=>{
+            if(this.state === FULFILLED){
+                onFulfilled(this.value)
+            }else{
+                onRejected(this.reason)
+            }
+        })
+        return p
+    }
+}
+
+// export default Promise
